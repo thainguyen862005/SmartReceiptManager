6d3a2b8cc1651238e9a4a7ac7c45b6d31c233492
IT29-KNK <23130247@st.hcmuaf.edu.vn>
2026-05-24 00:23:10 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/smartreceiptmanager/MainActivity.java
+++ app/src/main/java/com/example/smartreceiptmanager/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.smartreceiptmanager;
 
 import android.os.Bundle;
-import android.widget.Toast; // Thêm import này để xài Toast thông báo
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+
 import com.example.smartreceiptmanager.API_Stacistis.Statistics;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,14 +15,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Hiện tại chỉ mới có trang Thống kê nên tạm thời mở app lên sẽ load trang này trước
-        chuyenFragment(new Statistics());
+        // Mở Home TV2 trước
+        chuyenFragment(new ExpenseFragment());
 
-        // Bắt sự kiện các nút theo phong cách ngắn gọn (Inline) của bạn
-        findViewById(R.id.btnHome).setOnClickListener(v -> Toast.makeText(this, "Đang thiết kế trang Chủ!", Toast.LENGTH_SHORT).show());
-        findViewById(R.id.btnHistory).setOnClickListener(v -> Toast.makeText(this, "Đang thiết kế trang Lịch sử!", Toast.LENGTH_SHORT).show());
-        findViewById(R.id.btnStatistics).setOnClickListener(v -> chuyenFragment(new Statistics()));
-        findViewById(R.id.btnQRScan).setOnClickListener(v -> Toast.makeText(this, "Mở Camera Scan hóa đơn!", Toast.LENGTH_SHORT).show());
+        findViewById(R.id.btnHome).setOnClickListener(v ->
+                chuyenFragment(new ExpenseFragment())
+        );
+
+        findViewById(R.id.btnHistory).setOnClickListener(v ->
+                Toast.makeText(this, "Đang thiết kế trang Lịch sử!", Toast.LENGTH_SHORT).show()
+        );
+
+        findViewById(R.id.btnStatistics).setOnClickListener(v ->
+                chuyenFragment(new Statistics())
+        );
+
+        findViewById(R.id.btnQRScan).setOnClickListener(v ->
+                Toast.makeText(this, "Mở Camera Scan hóa đơn!", Toast.LENGTH_SHORT).show()
+        );
     }
 
     private void chuyenFragment(Fragment fragment) {
