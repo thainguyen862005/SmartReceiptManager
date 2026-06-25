9a5e8970d727b68055de44adfed8d2f0cf11f384
thainguyen862005 <23130296@st.hcmuaf.edu.vn>
2026-06-25 23:07:41 +0700

    fix compile and rebuild ./gradlew

--- app/src/main/java/com/example/smartreceiptmanager/expense/ExpenseListFragment.java
+++ app/src/main/java/com/example/smartreceiptmanager/expense/ExpenseListFragment.java
@@ -1,7 +1,5 @@
 package com.example.smartreceiptmanager.expense;
 
-import static android.os.Build.VERSION_CODES_FULL.R;
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
